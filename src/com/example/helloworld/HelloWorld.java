f7cc04e98599f38fcd40a409b11582f8d22bf34a
yasminr <yasminrosen8@gmail.com>
2018-11-14 10:00:24 +0200

    First commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    int i = 0;
+        for ( ; i < 10; ++i){
+            System.out.println("Hello, World!!");
+        }
+    }
+}
